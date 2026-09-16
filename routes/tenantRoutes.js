b243550336c36d1d3c704b3ebf28ec032bec69c8
Ajay <ajaygangadhar05@gmail.com>
2026-09-16 13:17:09 +0530

    Fix middleware import casing

--- routes/tenantRoutes.js
+++ routes/tenantRoutes.js
@@ -13,7 +13,7 @@ const {
 
 const authMiddleware = require("../middleware/authMiddleware");
 
-const authorizeRoles = require("../middleware/roleMiddleware");
+const authorizeRoles = require("../middleware/rolemiddleware");
 
 
 // =====================================================
