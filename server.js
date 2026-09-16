e5d78529bf4d492bf4ec6f806f0eb87c0f9a4150
Ajay <ajaygangadhar05@gmail.com>
2026-09-16 13:03:30 +0530

    Fix auth routes path

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("./routes/authroutes");
 const tenantRoutes = require("./routes/tenantRoutes");
 const userRoutes = require("./routes/userRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
